2b466ad439553faf2e711fe64eceef367dccd954
吴浩敏 <wuhm@yeastar.com>
2020-10-10 13:34:40 +0800

    复制

--- react/my-app/src/components/Icon/icon.tsx
+++ react/my-app/src/components/Icon/icon.tsx
@@ -1,31 +1,22 @@
-import React from 'react';
-import classNames from 'classnames';
-import {
-	FontAwesomeIcon,
-	FontAwesomeIconProps,
-} from '@fortawesome/react-fontawesome';
+import React from 'react'
+import classNames from 'classnames'
+import { FontAwesomeIcon, FontAwesomeIconProps } from '@fortawesome/react-fontawesome'
 
-export type ThemeProps =
-	| 'primary'
-	| 'secondary'
-	| 'success'
-	| 'info'
-	| 'warning'
-	| 'danger'
-	| 'light'
-	| 'dark';
+export type ThemeProps = 'primary' | 'secondary' | 'success' | 'info' | 'warning' | 'danger' | 'light' | 'dark'
 
-export type IconProps = FontAwesomeIconProps & {
-	theme?: ThemeProps;
-};
+export interface IconProps extends FontAwesomeIconProps {
+  theme? : ThemeProps
+}
 
 const Icon: React.FC<IconProps> = (props) => {
-	// icon-primary
-	const { className, theme, ...restProps } = props;
-	const classes = classNames('viking-icon', className, {
-		[`icon-${theme}`]: theme,
-	});
-	return <FontAwesomeIcon className={classes} {...restProps} />;
-};
+  // icon-primary
+  const { className, theme, ...restProps } = props
+  const classes = classNames('viking-icon', className, {
+    [`icon-${theme}`]: theme
+  })
+  return (
+    <FontAwesomeIcon className={classes} {...restProps} />
+  )
+}
 
-export default Icon;
+export default Icon
